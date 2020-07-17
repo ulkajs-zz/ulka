d89b63fa8df2443db4779a547895f328a48fd87d
acharyaroshanji <acharyaroshan2357@gmail.com>
2020-07-17 14:29:06 +0545

    import assets from .ulka files

--- src/parse/parseUlka.js
+++ src/parse/parseUlka.js
@@ -1,9 +1,27 @@
-const { parse } = require("ulka-parser");
+const path = require('path')
+const { parse } = require('ulka-parser')
+const generateFileName = require('../utils/generateName')
+
+const $assets = filePath => {
+  const fileName = generateFileName(
+    path.join(
+      process.cwd(),
+      path.parse(filePath).dir +
+        path.parse(filePath).name +
+        path.parse(filePath).ext
+    )
+  )
+  return path.join(path.sep, '__assets__', fileName) + path.parse(filePath).ext
+}
 
 const parseUlka = (ulkaTemplate, values) => {
+  values = {
+    ...values,
+    $assets
+  }
   return {
-    html: parse(ulkaTemplate, values),
-  };
-};
+    html: parse(ulkaTemplate, values)
+  }
+}
 
-module.exports = parseUlka;
+module.exports = parseUlka
