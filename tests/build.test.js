ea1eb093e7fdeb9d43393be9ccefbbfe93e1b4a8
acharyaroshanji <acharyaroshan2357@gmail.com>
2020-08-10 13:42:00 +0545

    more tests added returns

---
+++ tests/build.test.js
@@ -0,0 +1,33 @@
+require('colors')
+const fs = require('fs')
+const build = require('../bin/build')
+const absolutePath = require('../src/utils/absolutePath')
+const globalInfo = require('../src')
+
+const curDir = process.cwd()
+
+describe('build function: ', () => {
+  beforeEach(() => {
+    process.chdir(absolutePath('tests/resources/ulka_test'))
+  })
+
+  afterEach(() => {
+    process.chdir(curDir)
+  })
+
+  test('globalInfo should have info about content files', async () => {
+    await build()
+    const contentFiles = globalInfo.contentFiles.map(f => {
+      delete f.createFilePath
+      delete f.link
+      return f
+    })
+
+    expect(contentFiles).toMatchSnapshot()
+  })
+
+  test('bulid folder should be created', () => {
+    const buildExists = fs.existsSync(globalInfo.configs.buildPath)
+    expect(buildExists).toBe(true)
+  })
+})
---
+++ tests/resources/ulka_test/ulka-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetaData: {
+    title: 'UlkaJs',
+    description: 'This is a default starter for ulka js'
+  },
+  buildPath: 'build',
+  pagesPath: 'pages',
+  templatesPath: 'templates',
+
+  contents: {
+    path: 'contents',
+    generatePath: 'blog',
+    template: 'blog.ulka',
+    preParse: [],
+    postParse: []
+  }
+}
