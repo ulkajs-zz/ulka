d672015d59972695f7632e4564c43ae614795554
acharyaroshanji <acharyaroshan2357@gmail.com>
2020-08-12 21:48:01 +0545

    buildUlka function separated and pagesFiels removed from globalInfo

--- src/generate/generateUlka.js
+++ src/generate/generateUlka.js
@@ -33,44 +33,43 @@ async function generateFromUlka() {
     const ufd = fileDatas[i]
 
     try {
-      // Get filepath eg: \index.ulka or folder\file.ulka
-      const filePath = ufd.path.split(path.join('src', configs.pagesPath))[1]
-
-      // Prase filepath
-      const parsedPath = path.parse(filePath)
-
-      // filePath to create .html files eg: build\folder
-      let createFilePath = configs.buildPath + parsedPath.dir
-
-      // If name of file is not index, then create folder with fileName and change fileName to index
-      if (parsedPath.name !== 'index') {
-        createFilePath += '/' + parsedPath.name
-        parsedPath.name = 'index'
-      }
-
-      // Absolute Filepath to create html files
-      const absoluteFilePath = absolutePath(
-        `${createFilePath}/${parsedPath.name}.html`
-      )
-
-      // Prased html data
-      const html = (await ufd.data).html
-
-      // Create folder to generate html files
-      await mkdir(createFilePath)
-
-      globalInfo.pagesFiles.push({
-        html,
-        createFilePath
-      })
-
-      // Create html files
-      fs.writeFileSync(absoluteFilePath, html)
+      await buildFromUlka(ufd)
     } catch (e) {
       console.log(`>> Error while generating ${ufd.path}`.red)
       throw e
     }
   }
+}
+
+async function buildFromUlka(ufd) {
+  // Get filepath eg: \index.ulka or folder\file.ulka
+  const filePath = ufd.path.split(path.join('src', configs.pagesPath))[1]
+
+  // Prase filepath
+  const parsedPath = path.parse(filePath)
+
+  // filePath to create .html files eg: build\folder
+  let createFilePath = configs.buildPath + parsedPath.dir
+
+  // If name of file is not index, then create folder with fileName and change fileName to index
+  if (parsedPath.name !== 'index') {
+    createFilePath += '/' + parsedPath.name
+    parsedPath.name = 'index'
+  }
+
+  // Absolute Filepath to create html files
+  const absoluteFilePath = absolutePath(
+    `${createFilePath}/${parsedPath.name}.html`
+  )
+
+  // Prased html data
+  const html = (await ufd.data).html
+
+  // Create folder to generate html files
+  await mkdir(createFilePath)
+
+  // Create html files
+  fs.writeFileSync(absoluteFilePath, html)
 }
 
 module.exports = generateFromUlka
