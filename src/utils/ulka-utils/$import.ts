d371e18b0ad52fd78da84c3bbcf4f2930cb6ffc2
acharyaroshanji <acharyaroshan2357@gmail.com>
2020-09-16 22:49:04 +0545

    transforms images to base64

--- src/utils/ulka-utils/$import.ts
+++ src/utils/ulka-utils/$import.ts
@@ -1,10 +1,15 @@
 import path from "path"
+import { readFileSync } from "fs"
 import { fromMd, fromUlka } from "../transform-utils"
 
 const $import = async (rPath: string, values: any, filePath: string) => {
   const file = path.join(path.parse(filePath).dir, rPath)
-  if (file.endsWith(".ulka")) return await fromUlka(file, values)
-  else if (file.endsWith(".md")) return await fromMd(file)
+  const ext = path.parse(file).ext
+  if (ext === ".ulka") return await fromUlka(file, values)
+  else if (ext === ".md") return (await fromMd(file)).html
+  else if ([".png", ".jpg", ".jpeg"].includes(ext))
+    return "data:image/jpg;base64," + readFileSync(file, "base64")
+  else return readFileSync(file, "utf-8")
 }
 
 export default $import
