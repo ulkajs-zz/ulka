de555df080843b039e50d9432414cdfc7d6a19df
acharyaroshanji <acharyaroshan2357@gmail.com>
2020-09-22 20:06:47 +0545

    added changelog.md

--- tests/utils/ulka-source-utils/$assets.test.ts
+++ tests/utils/ulka-source-utils/$assets.test.ts
@@ -1,10 +1,8 @@
 import $assets from "../../../src/utils/ulka-source-utils/$assets"
 
 describe("$assets function", () => {
-  test("should have", () => {
+  test("should return hashed name", () => {
     const fileName = $assets("src/test/path", "../index.js")
-    expect(fileName).toMatchInlineSnapshot(
-      `"/__assets__/5033de5a2eccc46b3b728f15902592"`
-    )
+    expect(fileName.endsWith("index.js")).toBe(false)
   })
 })
