51cf8c523bd7911d4ae7309037565e4de966e214
Roshan Acharya <acharyaroshan2357@gmail.com>
2020-10-09 13:37:36 +0545

    feat: created renderUlka function

--- src/utils/render-utils.js
+++ src/utils/render-utils.js
@@ -1,5 +1,6 @@
 const fs = require("fs")
 const fm = require("front-matter")
+const { render } = require("ulka-parser")
 const { Remarkable } = require("remarkable")
 
 const md = new Remarkable()
@@ -9,20 +10,36 @@ const md = new Remarkable()
  *
  * @param {String} filePath - Full path to the markdown file.
  * @param {Object} values - Variables to be available inside markdown
- * @return {any}
+ * @return {{html: String, frontMatter: Object}}
  */
-function renderMarkdown(filePath) {
+function renderMarkdown(filePath, values) {
   const markdown = fs.readFileSync(filePath)
   const contents = fm(markdown)
 
   const html = md.render(contents.body)
 
+  const htmlFromUlka = renderUlka(html)
+
   return {
-    html,
+    html: htmlFromUlka,
     frontMatter: contents.attributes
   }
 }
 
+/**
+ * Render ulka as html
+ *
+ * @param {String} filePath
+ * @param {Object} values
+ * @return {String}
+ */
+function renderUlka(filePath, values) {
+  const template = fs.readFileSync(filePath)
+  const html = render(template, values)
+  return html
+}
+
 module.exports = {
-  renderMarkdown
+  renderMarkdown,
+  renderUlka
 }
