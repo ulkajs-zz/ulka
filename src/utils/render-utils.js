09eaef5981f2b1e9beb03b33a1375954fd5090da
Roshan Acharya <acharyaroshan2357@gmail.com>
2020-10-10 11:32:44 +0545

    feat: created render classed

--- src/utils/render-utils.js
+++ src/utils/render-utils.js
@@ -1,72 +1,5 @@
-const fs = require("fs")
-const path = require("path")
-const fm = require("front-matter")
-const { render } = require("ulka-parser")
-const { Remarkable } = require("remarkable")
-const { getConfigs } = require("./helpers")
-
-const md = new Remarkable()
-
-/**
- * Render markdown as html
- *
- * @param {String} filePath - Full path to the markdown file.
- * @param {Object} values - Variables to be available inside markdown
- * @param {Boolean} [isData]
- * @return {{html: String, frontMatter: Object, fields: Object}}
- */
-function renderMarkdown(filePath, values, isData = false) {
-  let markdown
-
-  if (isData) {
-    markdown = filePath
-  } else {
-    markdown = fs.readFileSync(filePath, "utf-8")
-  }
-
-  const data = {
-    markdown,
-    fields: {}
-  }
-
-  const { body, attributes } = fm(data.markdown)
-
-  data.body = body
-  data.frontMatter = attributes
-
-  data.html = md.render(data.body)
-
-  data.htmlFromUlka = renderUlka(data.html, {
-    frontMatter: data.frontMatter,
-    markdown: data.markdown,
-    fields: data.fields,
-    raw: data.html,
-    ...values
-  })
-
-  return {
-    html: data.htmlFromUlka,
-    frontMatter: data.markdown,
-    fields: data.fields
-  }
-}
-
-/**
- * Render ulka as html
- *
- * @param {String} filePath Full path to the ulka files
- * @param {Object} values - Variables to be available inside ulka
- * @return {String}
- */
-function renderUlka(filePath, values) {
-  const template = fs.readFileSync(filePath, "utf-8")
-
-  const context = ulkaContext(values, filePath)
-
-  const html = render(template, context)
-
-  return html
-}
+// const fs = require("fs")
+// const path = require("path")
 
 /**
  * Create context for ulka-parser from given values and default values
@@ -98,40 +31,24 @@ function ulkaContext(values, filePath) {
  * @return {any}
  */
 function $import(rPath, values, filePath) {
-  const imgExts = [".jpeg", ".jpg", ".png", ".gif", ".bmp", ".svg", ".webp"]
+  // const imgExts = [".jpeg", ".jpg", ".png", ".gif", ".bmp", ".svg", ".webp"]
+  // const file = path.join(path.parse(filePath).dir, rPath)
+  // const ext = path.parse(file).ext
+  // if (ext === ".ulka") {
+  //   return renderUlka(file, values)
+  // } else if (ext === ".md") {
+  //   return renderMarkdown(file, values).html
+  // } else if (imgExts.includes(ext)) {
+  //   const imgContent = fs.readFileSync(file, "base64")
+  //   return `data:image/${ext.substr(1)};base64,` + imgContent
+  // } else {
+  //   return fs.readFileSync(file, "utf-8")
+  // }
 
-  const file = path.join(path.parse(filePath).dir, rPath)
-
-  const ext = path.parse(file).ext
-
-  if (ext === ".ulka") {
-    return renderUlka(file, values)
-  } else if (ext === ".md") {
-    return renderMarkdown(file, values).html
-  } else if (imgExts.includes(ext)) {
-    const imgContent = fs.readFileSync(file, "base64")
-    return `data:image/${ext.substr(1)};base64,` + imgContent
-  } else {
-    return fs.readFileSync(file, "utf-8")
-  }
-}
-
-/**
- * Global Info
- *
- * @param {String} cwd
- * @return {any} globalInfo
- */
-function ulkaInfo(cwd) {
-  const configs = getConfigs(cwd)
-
-  return {
-    configs
-  }
+  return ""
 }
 
 module.exports = {
-  renderMarkdown,
-  renderUlka,
-  ulkaInfo
+  $import,
+  ulkaContext
 }
