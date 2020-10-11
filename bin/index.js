a2b790c8727cc74eac2ccaba84140418dd015989
Roshan Acharya <acharyaroshan2357@gmail.com>
2020-10-11 14:38:58 +0545

    feat: build command added in bin

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 
 const { program } = require("commander")
+const build = require("../src/ulka-cli/build")
 
 program.version(require("../package.json").version)
+
+program
+  .command("build")
+  .description("Build html from .md and .ulka files")
+  .action(() => build(process.cwd()))
 
 program.parse(process.argv)
