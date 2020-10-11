1160d6946ef816427a4ab4e3c0fd9eb80ed4a916
Roshan Acharya <acharyaroshan2357@gmail.com>
2020-10-11 10:52:17 +0545

    chore: generate contents from map in arguements

--- src/generate/generate.js
+++ src/generate/generate.js
@@ -1,17 +1,13 @@
 const path = require("path")
 const fs = require("fs")
-const { createPagesMap, createContentMap } = require("./create-map")
 const { mkdir } = require("../utils/ulka-fs")
 const Ulka = require("./Ulka")
+// const { createPagesMap, createContentMap } = require("./create-map")
 
 /**
- * @param {Object} ulkaInfo
- * @param {Object} contentsMap
- * @param {String} cwd
+ * @param {Object} pagesMap
  */
-function generatePages(ulkaInfo, contentsMap, cwd) {
-  const pagesMap = createPagesMap(ulkaInfo, { contents: contentsMap }, cwd)
-
+function generatePages(pagesMap) {
   Object.values(pagesMap).forEach(info => {
     mkdir(path.parse(info.buildPath).dir)
     fs.writeFileSync(info.buildPath, info.html)
@@ -19,12 +15,10 @@ function generatePages(ulkaInfo, contentsMap, cwd) {
 }
 
 /**
- * @param {Object} ulkaInfo
+ * @param {Object} contentsMap
  * @param {String} cwd
  */
-function generateContents(ulkaInfo, cwd) {
-  const contentsMap = createContentMap(ulkaInfo, cwd)
-
+function generateContents(contentsMap, cwd) {
   Object.keys(contentsMap).forEach(contentName => {
     const content = contentsMap[contentName]
 
@@ -32,7 +26,7 @@ function generateContents(ulkaInfo, cwd) {
       mkdir(path.parse(info.buildPath).dir)
 
       const templatePath = path.join(
-        ulkaInfo.configs.templatesPath,
+        contentsMap.configs.templatesPath,
         info.instance.contentInfo.template
       )
 
@@ -52,28 +46,6 @@ function generateContents(ulkaInfo, cwd) {
     })
   })
 }
-
-const cwd = path.join(process.cwd(), "example")
-
-const ulkaInfo = {
-  configs: {
-    buildPath: path.join(cwd, "build"),
-    templatesPath: path.join(cwd, "templates"),
-    pagesPath: path.join(cwd, "pages"),
-    contents: [
-      {
-        path: path.join(cwd, "contents"),
-        generatePath: "blog",
-        template: "template.ulka",
-        name: "blog"
-      }
-    ]
-  }
-}
-
-generateContents(ulkaInfo, cwd)
-
-generatePages(ulkaInfo, {}, cwd)
 
 module.exports = {
   generatePages,
