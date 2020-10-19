d95c5273164311e9552cdd28e3259e0227869323
Roshan Acharya <acharyaroshan2357@gmail.com>
2020-10-19 22:26:35 +0545

    feat: cache rendered html from ulka files

--- src/utils/build-utils.js
+++ src/utils/build-utils.js
@@ -225,6 +225,7 @@ function renderMarkdown(raw, info) {
   return md.render(raw)
 }
 
+const renderUlkaCache = {}
 /**
  * @param {String} raw Raw ulka
  * @param {Object} context Context
@@ -233,6 +234,14 @@ function renderMarkdown(raw, info) {
  * @return {String}
  */
 function renderUlka(raw, context, filePath, info) {
+  // while in development mode return cache if exists
+  if (info.task === "develop") {
+    const cache = renderUlkaCache[filePath]
+    if (cache && cache.raw === raw && JSON.stringify(context) === raw.context) {
+      return cache.html
+    }
+  }
+
   context = {
     ...context,
     $assets: rPath => $assets(rPath, filePath, info),
@@ -240,7 +249,12 @@ function renderUlka(raw, context, filePath, info) {
       return $import(rPath, { ...context, ...$values }, filePath, info)
     }
   }
-  return render(raw, context, { base: filePath })
+
+  const html = render(raw, context, { base: filePath })
+
+  renderUlkaCache[filePath] = { html, raw, context: JSON.stringify(context) }
+
+  return html
 }
 
 /**
